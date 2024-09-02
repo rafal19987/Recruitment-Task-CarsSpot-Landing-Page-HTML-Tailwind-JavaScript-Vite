856392a513652ec7e530765e4bac8d859cdc0f17
Rafixx <rafal987.1@o2.pl>
2024-09-02 19:45:30 +0200

    refactor: format document

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'airbnb',
-        'airbnb/hooks',
-        'eslint:recommended',
-        'plugin:prettier/recommended',
-    ],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    rules: {},
-    plugins: ['prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+  plugins: ['prettier'],
 }
