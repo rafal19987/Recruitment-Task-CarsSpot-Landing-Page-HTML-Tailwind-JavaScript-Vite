625f67fe02bcdb9abac0e5e2f788542a197e1a20
Rafixx <rafal987.1@o2.pl>
2024-09-02 19:46:21 +0200

    chore: extend taiwlind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        lg: '0rem',
+      },
+      screens: {
+        '2xl': '1200px',
+      },
+    },
+    extend: {
+      colors: {
+        bgColor: '#F7F7F7',
+        primary: '#282828',
+        secondary: '#F7F7F7',
+        accent: '#0147FF',
+        gray: '#F1F1F1',
+        dark: '#1D1D1B',
+        light: '#F2F2F2',
+      },
+      fontFamily: {
+        bebasNeue: ['"Bebas Neue"', ...defaultTheme.fontFamily.sans],
+        robotoFlex: ['"Roboto Flex"', ...defaultTheme.fontFamily.sans],
+        robotoCondensed: [
+          '"Roboto Condensed"',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
   },
   plugins: [],
-};
+}
