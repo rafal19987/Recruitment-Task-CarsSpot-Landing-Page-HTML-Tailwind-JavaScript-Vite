e89d9745e9cf4d8e220fb175070dc7081b5aef7a
Rafixx <rafal987.1@o2.pl>
2024-09-02 16:08:23 +0200

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb',
+        'airbnb/hooks',
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {},
+    plugins: ['prettier'],
+}
